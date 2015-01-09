c529bab85a534dfca5719fa0742e06810ff69dec
Marcelo Odir <marceloodir@gmail.com>
2015-01-08 21:39:11 -0300

    First dynamic page using MVC pattern

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,6 @@
+class VisitorsController < ApplicationController
+	def new
+		@primeiro = Primeiro.new
+		
+	end
+end
---
+++ app/models/primeiro.rb
@@ -0,0 +1,9 @@
+class Primeiro
+	def name
+		name = "Marcelo Odir"
+	end
+
+	def agora
+		agora = Date.today
+	end
+end
